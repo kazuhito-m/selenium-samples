7eb4c526d55c0f481ab085ccdb5da97d987af4bc
kazuhito_m <kazuhito.sumpic@gmail.com>
2014-07-11 18:14:01 +0900

    Refactoring. Reformatting source.

--- src/test/groovy/SeleniumSampleTest.groovy
+++ src/test/groovy/SeleniumSampleTest.groovy
@@ -1,31 +1,33 @@
 import org.junit.*
 import org.openqa.selenium.*
-import org.openqa.selenium.support.ui.*
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.support.ui.*
 
 class SeleniumSampleTest {
 
 
-	@BeforeClass
-	static void setup() {
-		// System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome")	// for Linux Chrome
-	}
+    @BeforeClass
+    static void setup() {
+        // System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome")	// for Linux Chrome
+    }
 
-	@Test
-	public void test1() {
-	
-		// WebDriver driver = new ChromeDriver()
-		FirefoxDriver driver = new FirefoxDriver()
+    @Test
+    public void test1() {
 
-		driver.get("http://www.htmlhifive.com/conts/web/view/Main/WebHome")
-		WebElement elTutorial = driver.findElement(By.linkText("⇒こちら"))
-		elTutorial.click();
+        // WebDriver driver = new ChromeDriver()
+        FirefoxDriver driver = new FirefoxDriver()
 
-		WebDriverWait wait = new WebDriverWait(driver, 10)
-	    	WebElement elTitle =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("document-title")))
-    		assert elTitle.getText() == "チュートリアル"
+        driver.get("http://www.htmlhifive.com/conts/web/view/Main/WebHome")
+        WebElement elTutorial = driver.findElement(By.linkText("⇒こちら"))
+        elTutorial.click();
 
-    		driver.quit();
-    		
-	}
+        WebDriverWait wait = new WebDriverWait(driver, 10)
+        WebElement elTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("document-title")))
+
+        // action & assertion
+        assert elTitle.getText() == "チュートリアル"
+
+        driver.quit();
+
+    }
 }
