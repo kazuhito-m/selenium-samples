276a7f2b9346caf0086364ca6907562e317ec152
kazuhito_m <kazuhito.sumpic@gmail.com>
2014-07-11 17:56:08 +0900

    Imprement. Added selnium sample source and implement.

---
+++ src/test/groovy/SeleniumSampleTest.groovy
@@ -0,0 +1,31 @@
+import org.junit.*
+import org.openqa.selenium.*
+import org.openqa.selenium.support.ui.*
+import org.openqa.selenium.firefox.FirefoxDriver
+
+class SeleniumSampleTest {
+
+
+	@BeforeClass
+	static void setup() {
+		// System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome")	// for Linux Chrome
+	}
+
+	@Test
+	public void test1() {
+	
+		// WebDriver driver = new ChromeDriver()
+		FirefoxDriver driver = new FirefoxDriver()
+
+		driver.get("http://www.htmlhifive.com/conts/web/view/Main/WebHome")
+		WebElement elTutorial = driver.findElement(By.linkText("⇒こちら"))
+		elTutorial.click();
+
+		WebDriverWait wait = new WebDriverWait(driver, 10)
+	    	WebElement elTitle =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("document-title")))
+    		assert elTitle.getText() == "チュートリアル"
+
+    		driver.quit();
+    		
+	}
+}
